10cabbf6d7d44ebb9e90b904ca88a12c48874e33
“Patrick <pctennislessons@gmail.com>
2019-08-15 20:25:05 -0600

    router lesson completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 import Home from './components/Home'
 import About from './components/About'
@@ -19,16 +20,25 @@ class App extends Component {
   render() {
 
     return (
+      <Router>
 
-      <div className="nav">
-        <Link className="link" to="/">Home</Link>
-        <Link className="link" to="/about/">About</Link>
-        <Link className="link" to="/list/">List</Link>
-      </div>
+          <div className="nav">
+            <Link className="link" to="/">Home</Link>
+            <Link className="link" to="/about/">About</Link>
+            <Link className="link" to="/list/">List</Link>
+          </div>
 
+        <Switch>
+            <Route path="/" exact component={ Home } />
+            <Route path="/about/" component={ About } />
+            <Route path="/list/" render={(props) => <List {...props} animals={this.state.list} /> } />
+            <Route component={ AppNotFound } />
+        </Switch>
+
+      </Router>
     );
   }
-}
+} 
 
 export default App;
 
--- src/components/List.js
+++ src/components/List.js
@@ -5,12 +5,20 @@ class List extends Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            inHome: false
         }
     }
-
+    handleRedirect = () => {
+        this.setState({ inHome: true })
+    }
+    
+    historyPush = () => {
+        this.props.history.push("/about/")
+    }
     render() {
-
+        if(this.state.inHome){
+            return <Redirect to="/" />
+        }
         const animalList = this.props.animals.map((animal, idx) => {
             return <h1 key={ idx }>{ animal }</h1>
         })
@@ -18,9 +26,9 @@ class List extends Component {
             <div className="list">
                 { animalList }
 
-                <button >Redirect Component Button</button>
+                <button onClick={ this.handleRedirect } >Redirect Component Button</button>
 
-                <button >History Push Button</button>
+                <button onClick={this.historyPush}>History Push Button</button>
             </div >
 
         );
